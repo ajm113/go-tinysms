f3faccadd12788c732c6f46bce1840372e18b657
Andrew McRobb <andrewmcrobb@gmail.com>
2020-04-17 19:41:38 -0400

    Improved Options and Makefile.

--- Makefile
+++ Makefile
@@ -7,13 +7,6 @@ fmt:
 	@go fmt .
 
 
-.PHONY: tag
-
-tag:
-	@git tag `grep -P '^\tversion = ' echo.go|cut -f2 -d'"'`
-	@git tag|grep -v ^v
-
-
 .PHONY: test
 
 test:
--- options.go
+++ options.go
@@ -1,9 +1,14 @@
 package tinysms
 
-// Options @TODO Write me!
+// Options SMTP Connection strings.
 type Options struct {
-	Addr        string
-	Username    string
-	Password    string
+	// Addr we are connecting to. e.g: "smtp.gmail.com:587"
+	Addr string
+
+	// Username Sometimes email address or username used to login
+	Username string
+	Password string
+
+	// FromAddress Use custom from address if service supports it. Sometimes they will ignore this.
 	FromAddress string
 }
